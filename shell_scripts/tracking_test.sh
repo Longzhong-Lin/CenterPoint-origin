20378c65f9935dae57b5c00ce95b989ed790821a
Lin-Longzhong <linlongzhong2000@zju.edu.cn>
2023-03-29 21:55:55 +0800

    test om model

--- shell_scripts/tracking_test.sh
+++ shell_scripts/tracking_test.sh
@@ -34,9 +34,29 @@ python tools/nusc_tracking/pub_test.py \
 --root data/nuScenes-mini/v1.0-test
 }
 
+om_val_mini()
+{
+python tools/nusc_tracking/pub_test.py \
+--work_dir work_dirs/centerpoint_pillar_pretrain/val-mini/om/tracking \
+--checkpoint work_dirs/centerpoint_pillar_pretrain/val-mini/om/infos_val_10sweeps_withvelo_filter_True.json \
+--version v1.0-mini \
+--root data/nuScenes-mini
+}
+
+om_testing()
+{
+python tools/nusc_tracking/pub_test.py \
+--work_dir work_dirs/centerpoint_pillar_pretrain/test/om/tracking \
+--checkpoint work_dirs/centerpoint_pillar_pretrain/test/om/infos_test_10sweeps_withvelo.json \
+--version v1.0-test \
+--root data/nuScenes-mini/v1.0-test
+}
 
 # val_mini
 # testing
 
 # onnx_val_mini
-onnx_testing
+# onnx_testing
+
+# om_val_mini
+om_testing
